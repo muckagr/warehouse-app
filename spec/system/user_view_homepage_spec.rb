f510e0e647a46b382c50f27b9f13b393c9877b1f
Arthur Patrocinio <arthurafk@gmail.com>
2022-05-01 03:24:15 -0300

    usuario ve galpoes na tela inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -10,4 +10,37 @@ describe 'Usuário visita tela inicial' do
         #Assert
         expect(page).to have_content('Galpões & Estoque')
     end
+    
+    it 'e vê os galpões cadastrados' do
+        #Arrange
+        Warehouse.create(name: 'Rio', codigo: "SDU", cidade: "Rio de Janeiro", area: 60_000)
+        Warehouse.create(name: 'Maceio', codigo: "MCZ", cidade: "Maceio", area: 50_000)
+
+        #Act
+        visit('/')
+
+        #Assert
+        expect(page).not_to have_content('Não há galpões cadastrados')
+
+        expect(page).to have_content('Rio')
+        expect(page).to have_content('Cidade: Rio de Janeiro')
+        expect(page).to have_content('Código: SDU')
+        expect(page).to have_content('60000 m2')
+
+        expect(page).to have_content('Maceio')
+        expect(page).to have_content('Cidade: Maceio')
+        expect(page).to have_content('Código: MCZ')
+        expect(page).to have_content('50000 m2')
+    end
+
+    it 'e não existem galpões cadastrados' do
+        #Arrange
+
+
+        #Act
+        visit('/')
+        
+        #Assert
+        expect(page).to have_content('Não há galpões cadastrados')
+    end
 end
