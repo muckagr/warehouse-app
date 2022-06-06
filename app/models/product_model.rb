0c300110b7edd3203a213ed818e99708d10ce9b7
muckagr <arthurafk@gmail.com>
2022-06-06 00:44:32 -0300

    com model order_item e associação do tipo has_many through com order e product_model

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,6 +1,8 @@
 class ProductModel < ApplicationRecord
+  belongs_to :supplier
+  has_many :order_items
+
   validates :name, :weight, :width, :height, :depth, :sku, :supplier_id, presence: true
   validates :width, :weight, :height, :depth, comparison: { greater_than: 0 }
   validates :sku, uniqueness: true
-  belongs_to :supplier
 end
--- spec/system/orders/user_view_own_orders_spec.rb
+++ spec/system/orders/user_view_own_orders_spec.rb
@@ -56,7 +56,6 @@ describe 'usuário vê seus próprios pedidos' do
         formated_date = I18n.localize(1.day.from_now.to_date)
         expect(page).to have_content("Data Prevista de Entrega: #{formated_date}")
         expect(page).to have_content('Pendente')
-
     end
 
     it 'e não visita pedidos de outros usuários' do
@@ -80,5 +79,29 @@ describe 'usuário vê seus próprios pedidos' do
         expect(page).to have_content('Você não possui acesso a esse pedido.')
     end
 
+    it 'e vê itens do pedido' do
+        user = User.create!(name: "Arthur", email: "arthur@email.com", password: "password")
+        warehouse = Warehouse.create!(name: 'Galpão Aeroporto SP', code: "GRU", city: "Guarulhos", 
+                    area: 100_000, adress: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+                    description: 'Galpão destinado para cargas internacionais')
+        supplier = Supplier.create!(corporate_name: "Manaós Industria", brand_name: "Manaós Soluções Industriais", 
+                    registration_number: "8009461400011", full_adress: "Vieralves, 255", city: "Manaus", state: "AM", 
+                    email: "manaos.solucoes.ind@gmail.com")
+        order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+        product_1 = ProductModel.create!(name: 'TV 32', weight: 8000, width: 70, height: 45, depth: 10, sku:'TV32-MAO-XPTO90', supplier: supplier)
+        product_2 = ProductModel.create!(name: 'Soundbar 7.1', weight: 3000, width: 80, height: 15, depth: 20, sku:'SOU71-SAMSU-NOIZ77', supplier: supplier)
+        product_3 = ProductModel.create!(name: 'Webcam 1080p Logitech', weight: 200, width: 10, height: 7, depth: 5, sku:'WEBLOG3072', supplier: supplier)
+        order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now)
+        OrderItem.create!(product_model: product_1, order: order, quantity: 19)
+        OrderItem.create!(product_model: product_2, order: order, quantity: 12)
 
+        login_as(user)
+        visit(root_path)
+        click_on('Meus Pedidos')
+        click_on(order.code)
+
+        expect(page).to have_content('Itens do Pedido')
+        expect(page).to have_content('19 x TV 32')
+        expect(page).to have_content('12 x Soundbar 7.1')
+    end
 end
